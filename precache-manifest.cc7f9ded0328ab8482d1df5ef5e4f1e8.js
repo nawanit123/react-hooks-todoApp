b9af97398a6bb9488b233bc91bb2802b1986e98b
Nawanit <sharma.nawanit123@gmail.com>
2020-10-12 19:56:42 +0530

    Updates

---
+++ precache-manifest.cc7f9ded0328ab8482d1df5ef5e4f1e8.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e00f7ab640655952c122229b192a65",
+    "url": "/react-hooks-todoApp/index.html"
+  },
+  {
+    "revision": "37bf13a44524102aabc2",
+    "url": "/react-hooks-todoApp/static/css/2.d3fb9138.chunk.css"
+  },
+  {
+    "revision": "6eb64c7f83af22f5cae0",
+    "url": "/react-hooks-todoApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "37bf13a44524102aabc2",
+    "url": "/react-hooks-todoApp/static/js/2.ab439908.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-hooks-todoApp/static/js/2.ab439908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eb64c7f83af22f5cae0",
+    "url": "/react-hooks-todoApp/static/js/main.e8dd3410.chunk.js"
+  },
+  {
+    "revision": "5e803a5945889ef87377",
+    "url": "/react-hooks-todoApp/static/js/runtime-main.2ea3d636.js"
+  },
+  {
+    "revision": "c762d850e2e8d0e29047608715196736",
+    "url": "/react-hooks-todoApp/static/media/roboto-all-400-italic.c762d850.woff"
+  },
+  {
+    "revision": "a91ad097d24828af724d4fee36a063ed",
+    "url": "/react-hooks-todoApp/static/media/roboto-all-400-normal.a91ad097.woff"
+  },
+  {
+    "revision": "8266321ab43353bcd147ad67600d165c",
+    "url": "/react-hooks-todoApp/static/media/roboto-cyrillic-400-italic.8266321a.woff2"
+  },
+  {
+    "revision": "8bb64952764a884d67019b3486296ab9",
+    "url": "/react-hooks-todoApp/static/media/roboto-cyrillic-400-normal.8bb64952.woff2"
+  },
+  {
+    "revision": "b3e580d221a4722c959155878ab94210",
+    "url": "/react-hooks-todoApp/static/media/roboto-cyrillic-ext-400-italic.b3e580d2.woff2"
+  },
+  {
+    "revision": "4743c758a952f2bd4a35d4e42afc002b",
+    "url": "/react-hooks-todoApp/static/media/roboto-cyrillic-ext-400-normal.4743c758.woff2"
+  },
+  {
+    "revision": "469a78405c3ae073ba769321fa0584f3",
+    "url": "/react-hooks-todoApp/static/media/roboto-greek-400-italic.469a7840.woff2"
+  },
+  {
+    "revision": "c1e9793c84cb26c44ef2a2cf8b6f49ce",
+    "url": "/react-hooks-todoApp/static/media/roboto-greek-400-normal.c1e9793c.woff2"
+  },
+  {
+    "revision": "801b64f75ae26dab9c36f00629ebdad0",
+    "url": "/react-hooks-todoApp/static/media/roboto-greek-ext-400-italic.801b64f7.woff2"
+  },
+  {
+    "revision": "182ee6a4872ca8fa78048951b1561a5c",
+    "url": "/react-hooks-todoApp/static/media/roboto-greek-ext-400-normal.182ee6a4.woff2"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/react-hooks-todoApp/static/media/roboto-latin-400-italic.51521a2a.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/react-hooks-todoApp/static/media/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "dc3871f486e189164db4b98968330bc4",
+    "url": "/react-hooks-todoApp/static/media/roboto-latin-ext-400-italic.dc3871f4.woff2"
+  },
+  {
+    "revision": "455200cb007fe1212c668721d827c691",
+    "url": "/react-hooks-todoApp/static/media/roboto-latin-ext-400-normal.455200cb.woff2"
+  },
+  {
+    "revision": "9780bde87bceec579eaf16bddad47615",
+    "url": "/react-hooks-todoApp/static/media/roboto-vietnamese-400-italic.9780bde8.woff2"
+  },
+  {
+    "revision": "a8be5b46d06bb541b0968196ee5e6bb8",
+    "url": "/react-hooks-todoApp/static/media/roboto-vietnamese-400-normal.a8be5b46.woff2"
+  }
+]);
